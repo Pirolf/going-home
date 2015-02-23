f46fe833314d2320ccd77bac20fd6f665ebc88f8
Pirolf <yuchengtu.uwm@gmail.com>
2015-02-22 21:03:32 -0600

    add project

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//forward: +x
+//left: +z
+public class PlayerController : MonoBehaviour {
+	public float forwardSpeed = 0.5f;
+	public float sideSpeed = 0.5f;
+	public float rotYSpeed = 100f;
+	private Vector3 deltaTranslate;
+	//look around
+	// Use this for initialization
+	void Awake(){
+		deltaTranslate = Vector3.zero;
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float fb = Input.GetAxis("Vertical");
+		float lr = Input.GetAxis("Horizontal");
+		float deltaFB = forwardSpeed * fb * Time.deltaTime;
+		float deltaLR = sideSpeed * lr * Time.deltaTime;
+		transform.position += transform.forward * deltaFB;
+		transform.position += transform.right * deltaLR;
+		//transform.Translate(deltaFB, 0, deltaLR);
+
+		
+	}
+}
