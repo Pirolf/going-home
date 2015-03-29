dbb0fde1b3617952142bba149481b1fa7f89076a
Pirolf <yuchengtu.uwm@gmail.com>
2015-03-29 18:27:26 -0500

    nav mesh agent works with dialogue

---
+++ Assets/scripts/NPCScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCScript : MonoBehaviour {
+
+	public float temper;
+	public float courage;
+	public bool beggedToday = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnEnable(){
+		InitPersonality();
+		Debug.Log(temper);
+	}
+	public void InitPersonality(){
+		temper = UnityEngine.Random.Range(0,10) / 10.0f;
+		courage = UnityEngine.Random.Range(0,1);
+	}
+	void OnCollisionEnter(Collision other){
+		Debug.Log("npc collides player");
+		if(other.gameObject.name.Equals("homelessPlayer")){
+			
+			GameControl.player.GetComponent<PlayerController>().disableMove = true;
+			GameControl.gameState = (int)GameControl.GameState.HumanInteraction;
+			
+			beggedToday = true;
+			DialogueSystem.dialogueSystem.StartInteraction(temper, beggedToday);
+			
+		}
+	}
+}
--- Assets/scripts/NPC_NavMeshAgent.cs
+++ Assets/scripts/NPC_NavMeshAgent.cs
@@ -7,11 +7,18 @@ public class NPC_NavMeshAgent : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		nma = gameObject.GetComponent<NavMeshAgent>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		nma.SetDestination(target.position);
+		if(GameControl.gameState == (int) GameControl.GameState.PlayerNavigating){
+
+			nma.SetDestination(target.position);
+			nma.Resume();
+		}else{
+			nma.Stop();
+		}
 		//transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
 	}
 }
