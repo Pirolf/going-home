8460ee913db64f3b02c096d08268dbcd0780939c
Pirolf <yuchengtu.uwm@gmail.com>
2015-03-29 17:58:35 -0500

    nav mesh agent, baked, works

---
+++ Assets/scripts/NPC_NavMeshAgent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_NavMeshAgent : MonoBehaviour {
+	private NavMeshAgent nma;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		nma = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nma.SetDestination(target.position);
+		//transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+	}
+}
